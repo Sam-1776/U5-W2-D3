522e6661071443f6b6afea39d692df7dfc10e802
Samuele Simeone <69748013+Sam-1776@users.noreply.github.com>
2024-02-14 14:22:00 +0100

    First commit

---
+++ src/main/java/samuelesimeone/eserciziou5w2d4/Eserciziou5w2d4Application.java
@@ -0,0 +1,13 @@
+package samuelesimeone.eserciziou5w2d4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eserciziou5w2d4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eserciziou5w2d4Application.class, args);
+	}
+
+}
---
+++ src/test/java/samuelesimeone/eserciziou5w2d4/Eserciziou5w2d4ApplicationTests.java
@@ -0,0 +1,13 @@
+package samuelesimeone.eserciziou5w2d4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eserciziou5w2d4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
